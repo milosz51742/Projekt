cbc2a3a964c9d9a175fdfcba4890354d0f02712f
milosz51742 <167362153+milosz51742@users.noreply.github.com>
2024-06-05 18:43:30 +0200

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import argparse
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='Program description.')
-    parser.add_argument('--input', type=str, required=True, help='Input file path')
-    parser.add_argument('--output', type=str, required=True, help='Output file path')
-    parser.add_argument('--format', type=str, choices=['json', 'yaml', 'xml'], required=True, help='File format')
-    return parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(description="Example program")
+    parser.add_argument('--input', required=True, help='Input file')
+    parser.add_argument('--output', required=True, help='Output file')
+    parser.add_argument('--format', required=True, choices=['json', 'yaml', 'xml'], help='Format of the file')
+
+    args = parser.parse_args()
+
+    print(f"Input file: {args.input}")
+    print(f"Output file: {args.output}")
+    print(f"Format: {args.format}")
 
 if __name__ == "__main__":
-    args = parse_arguments()
-    print(f'Input file: {args.input}')
-    print(f'Output file: {args.output}')
-    print(f'Format: {args.format}')
+    main()
